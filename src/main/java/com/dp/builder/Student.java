c90a1cf3d7e5d6353f0969294599627649135636
abhinay pothu <abhinaypothu@abhinays-MacBook-Air.local>
2025-05-10 13:24:41 +0530

    BuilderExample

---
+++ src/main/java/com/dp/builder/Student.java
@@ -0,0 +1,30 @@
+package com.dp.builder;
+
+public class Student implements StudentInterf {
+    String name;
+    int age;
+    int classNum;
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public void setClass(int classNum) {
+        this.classNum = classNum;
+    }
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", classNum=" + classNum +
+                '}';
+    }
+}
---
+++ src/main/java/com/dp/builder/StudentInterf.java
@@ -0,0 +1,7 @@
+package com.dp.builder;
+
+public interface StudentInterf {
+    void setName(String name);
+    void setAge(int age);
+    void setClass(int classNum);
+}
